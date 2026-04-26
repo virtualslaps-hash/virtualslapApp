3fec3a26cf2218fa6e4f89a04b8fe14720bb5705
navneet18 <navgpt18@gmail.com>
2026-04-26 21:29:55 +0530

    removed cursor styling

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
   useReveal()
   return (
     <>
-      <Cursor />
+      {/* <Cursor /> */}
       <DotCanvas />
       <div className="radial-center" />
       <Navbar />
